75c6c4310f09dd74ea2430a798b28aba4c945fa2
Anis BOUCHEKHIMA <bouchekhima.anis@gmail.com>
2019-04-29 17:25:56 +0100

    add reorganize file structure

--- app/index.js
+++ app/index.js
@@ -1,9 +1,7 @@
 import Vue from 'nativescript-vue'
 import App from '@/App'
-import '@/routing/router'
-
-import store from '@/state/store';
-Vue.prototype.$store = store
+import '@/routing'
+import '@/state'
 
 // import VueDevtools from 'nativescript-vue-devtools'
 //
@@ -13,7 +11,6 @@ Vue.prototype.$store = store
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production')
-
 
 const app = new Vue({
 	render: h => h(App)
---
+++ app/state/index.js
@@ -0,0 +1,4 @@
+import Vue from 'nativescript-vue'
+import store from './store';
+
+Vue.prototype.$store = store
