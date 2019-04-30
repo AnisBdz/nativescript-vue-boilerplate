32bf0354f59eb9e027d6dc7e68f6e794910186d6
Anis BOUCHEKHIMA <bouchekhima.anis@gmail.com>
2019-04-30 02:45:32 +0100

    add handler for unauthorization

--- app/services/api.js
+++ app/services/api.js
@@ -1,9 +1,26 @@
 import axios from 'axios'
+import Vue from 'nativescript-vue'
 
+// create custom axios instance
 const api = axios.create({
 	baseURL: 'http://localhost:3333'
 })
 
+// setup defaults
 instance.defaults.headers.common['Authorization'] = 'AUTH_TOKEN'
+
+// create vue instance to handle routing
+const vue = new Vue()
+
+// setup unauthorozation interseptor
+const interseptor = api.interceptors.request.use(() => {}, error => {
+	if (error.response) {
+    	if (error.response.status == 401) { // 401 Unauthorized
+			vue.$store.dispatch('auth/logout')
+			vue.$navigator.navigate('/login')
+		}
+	}
+})
+
 
 export default api
